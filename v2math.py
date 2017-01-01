0920761124af3126734415bb73cba9dd79a68cc4
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2017-01-01 20:16:00 +0300

    minor v2math changes

--- v2math.py
+++ v2math.py
@@ -4,18 +4,26 @@ import numpy as np
 
 
 def v2norm(vector):
+    # Длина вектора
     return sqrt(vector[0] * vector[0] + vector[1] * vector[1])
 
 
+def v2sqr_norm(vector):
+    # Квадрат длины вектора ( для ускорения вычислений )
+    return vector[0] * vector[0] + vector[1] * vector[1]
+
+
 def v2unit(vector):
+    # Единичный вектор
     v_norm = v2norm(vector)
     return vector / v_norm if v_norm != 0 else np.array((0., 0.))
 
 
 def v2normal(vector):
-    # turn to 90 degrees
+    # Нормаль к вектору
     return v2unit(np.array((vector[1], -vector[0])))
 
 
 def v2reflect(vector, normal):
+    # Вектор-отражение исходного от нормали
     return -vector + 2 * np.dot(vector, normal) * normal
